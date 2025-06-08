af25cdde58dc8afaa14cd0fe44f10e578cf4b1c0
Connor Rowe <116033513+ConnorGIT5@users.noreply.github.com>
2025-06-08 02:29:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Page is working correctly. \n Please work."
+    return "Page is working correctly. \n Page takes some time to update content even after the logs confirm that build/deploy was been successfully completed."
